d14d76aebb26cf2a5b8bbb432d2b6f09717506fa
developiaa <developiaa@gmail.com>
2024-04-05 22:00:00 +0900

    first commit

---
+++ src/main/java/pro/developia/reactor/SpringReactorStudyApplication.java
@@ -0,0 +1,13 @@
+package pro.developia.reactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactorStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactorStudyApplication.class, args);
+    }
+
+}
