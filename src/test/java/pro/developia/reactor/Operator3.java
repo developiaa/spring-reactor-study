f3ed8ff5815c1eecdd2d6b07a6729b758b8bbdf9
developiaa <developiaa@gmail.com>
2024-04-09 23:00:00 +0900

    test: operator #3

---
+++ src/test/java/pro/developia/reactor/Operator3.java
@@ -0,0 +1,34 @@
+package pro.developia.reactor;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class Operator3 {
+    // count
+    public Mono<Long> fluxCount() {
+        return Flux.range(1, 10).count();
+    }
+
+    // distinct
+    public Flux<String> fluxDistinct() {
+        return Flux.fromIterable(List.of("a", "b", "a", "b", "c"))
+                .distinct();
+    }
+
+    // reduce
+    public Mono<Integer> fluxReduce() {
+        return Flux.range(1, 10)
+                .reduce((i, j) -> i + j)
+                .log();
+    }
+
+    // groupby
+    public Flux<Integer> fluxGroupBy() {
+        return Flux.range(1, 10)
+                .groupBy(i -> (i % 2 == 0) ? "even" : "odd")
+                .flatMap(group -> group.reduce(Integer::sum))
+                .log();
+    }
+}
