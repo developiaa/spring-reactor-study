dac42da252dd03037832aeec3f39033f9f326007
developiaa <developiaa@gmail.com>
2024-05-01 10:10:00 +0900

    feat: flow 모듈 추가

---
+++ flow/src/main/java/pro/developia/flow/FlowApplication.java
@@ -0,0 +1,13 @@
+package pro.developia.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowApplication.class, args);
+	}
+
+}
