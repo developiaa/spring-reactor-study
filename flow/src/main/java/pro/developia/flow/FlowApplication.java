5bc25ae17674214440bb2b92515bae13af007ff2
developiaa <developiaa@gmail.com>
2024-05-06 09:30:00 +0900

    feat: 대기열 스케쥴러 개발

--- flow/src/main/java/pro/developia/flow/FlowApplication.java
+++ flow/src/main/java/pro/developia/flow/FlowApplication.java
@@ -2,7 +2,9 @@ package pro.developia.flow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class FlowApplication {
 
